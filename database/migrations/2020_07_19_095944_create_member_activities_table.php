cebe6344a59490da2389dec5da89281de23e1fd5
attraverso <dev>
2020-07-19 12:34:30 +0200

    initial models compiled

---
+++ database/migrations/2020_07_19_095944_create_member_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('membership_for_year', 4);
+            $table->date('request_date')->nullable();
+            $table->string('membership_type', 15);
+            $table->date('membership_expiration')->nullable();
+            $table->boolean('class');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_activities');
+    }
+}
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,3 @@
+@extends('layouts.dashboard')
+
+@section('page-title', 'Bacheca Admin')
